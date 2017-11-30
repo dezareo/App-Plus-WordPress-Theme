219baf194edc25daf2accd5e12129027e9c3cd8e
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2017-11-30 19:44:44 +0100

    Create template part content-home-service.php

---
+++ template-parts/content-home-service.php
@@ -0,0 +1,52 @@
+<?php
+
+
+?>
+
+<!-- sevice section -->
+
+<section id="service">
+        <div class="container">
+            <div class="service-wrapper">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="block wow fadeInRight" data-wow-delay="1s">
+                            <div class="icon">
+                               <i class="fa fa-desktop"></i> 
+                            </div>
+                            
+                            <h3>Fully Customizable</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="block wow fadeInRight" data-wow-delay="1.3s">
+                            <div class="icon">
+                                <i class="fa  fa-paper-plane"></i>
+                            </div>
+                            <h3>Gorgeous design</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="block wow fadeInRight" data-wow-delay="1.6s">
+                            <div class="icon">
+                                <i class="fa  fa-file-text"></i>
+                            </div>
+                            <h3>Well documented.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                        </div>
+                    </div>
+                     <div class="col-md-3 col-sm-6">
+                        <div class="block wow fadeInRight" data-wow-delay="1.9s">
+                            <div class="icon">
+                                <i class="fa  fa-laptop"></i>
+                            </div>
+                            <h3>Responsive Design.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
