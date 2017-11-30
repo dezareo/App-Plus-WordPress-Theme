a2a7900cea46f117c6e762ee3ed6c62243e618dd
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2017-11-30 21:09:29 +0100

    content-home-service.php
    Create get_fields variables and import into html page

--- template-parts/content-home-service.php
+++ template-parts/content-home-service.php
@@ -1,6 +1,16 @@
 <?php
-
-
+$section_box_icon_1 = get_field('section_box_icon_1'); 
+$section_box_title_1 = get_field('section_box_title_1'); 
+$section_box_content_1 = get_field('section_box_content_1'); 
+$section_box_icon_2 = get_field('section_box_icon_2'); 
+$section_box_title_2 = get_field('section_box_title_2'); 
+$section_box_content_2 = get_field('section_box_content_2');
+$section_box_icon_3 = get_field('section_box_icon_3'); 
+$section_box_title_3 = get_field('section_box_title_3'); 
+$section_box_content_3 = get_field('section_box_content_3');
+$section_box_icon_4 = get_field('section_box_icon_4'); 
+$section_box_title_4 = get_field('section_box_title_4'); 
+$section_box_content_4 = get_field('section_box_content_4');
 ?>
 
 <!-- sevice section -->
@@ -12,38 +22,38 @@
                     <div class="col-md-3 col-sm-6">
                         <div class="block wow fadeInRight" data-wow-delay="1s">
                             <div class="icon">
-                               <i class="fa fa-desktop"></i> 
+                               <?php echo $section_box_icon_1; ?> 
                             </div>
                             
-                            <h3>Fully Customizable</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                            <h3><?php echo $section_box_title_1; ?></h3>
+                            <p><?php echo $section_box_content_1; ?></p>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-6">
                         <div class="block wow fadeInRight" data-wow-delay="1.3s">
                             <div class="icon">
-                                <i class="fa  fa-paper-plane"></i>
+                                <?php echo $section_box_icon_2; ?>
                             </div>
-                            <h3>Gorgeous design</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                            <h3><?php echo $section_box_title_2; ?></h3>
+                            <p><?php echo $section_box_content_2; ?></p>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-6">
                         <div class="block wow fadeInRight" data-wow-delay="1.6s">
                             <div class="icon">
-                                <i class="fa  fa-file-text"></i>
+                                <?php echo $section_box_icon_3; ?>
                             </div>
-                            <h3>Well documented.</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                            <h3><?php echo $section_box_title_3; ?></h3>
+                            <p><?php echo $section_box_content_3; ?></p>
                         </div>
                     </div>
                      <div class="col-md-3 col-sm-6">
                         <div class="block wow fadeInRight" data-wow-delay="1.9s">
                             <div class="icon">
-                                <i class="fa  fa-laptop"></i>
+                                <?php echo $section_box_icon_4; ?>
                             </div>
-                            <h3>Responsive Design.</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, amet.</p>
+                            <h3><?php echo $section_box_title_4; ?></h3>
+                            <p><?php echo $section_box_content_4; ?></p>
                         </div>
                     </div>
                 </div>
