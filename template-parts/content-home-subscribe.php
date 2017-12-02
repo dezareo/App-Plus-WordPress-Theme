db1024d900bc20085609405a57decc15fcf91a1e
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2017-12-02 11:10:46 +0100

    work on subscribe form

--- template-parts/content-home-subscribe.php
+++ template-parts/content-home-subscribe.php
@@ -1,4 +1,6 @@
 <?php
+$subscribe_title = get_field('subscribe_title');
+$subscribe_subtitle = get_field('subscribe_subtitle');
 $subscribe_form = get_field('subscribe_form');
 
 ?>
@@ -9,22 +11,22 @@ $subscribe_form = get_field('subscribe_form');
                 <div class="col-md-12 wow fadeInDown" data-wow-delay=".8s">
                     <div class="block">
                         <div class="title text-center">
-                            <h2>Stay Connected</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, non!</p>
+                            <h2><?php echo $subscribe_title; ?></h2>
+                            <p><?php echo $subscribe_subtitle; ?></p>
                         </div>
                         
                        
 
                         <form class="form-inline text-center col-sm-12 col-xs-12" role="form">
                              
-                             <?php echo do_shortcode($subscribe_form); ?>
+                             <?php //echo do_shortcode($subscribe_form); ?>
 
-                         <!--    <div class="form-group">
+                         <div class="form-group">
                                  <input type="text" class="form-control" id="signup-form" >  
                             </div>
                             <a href="" type="submit" class="btn btn-default btn-signup">
                                 <i class="fa fa-paper-plane"></i>
-                            </a> -->
+                            </a>
 
                         </form>
                     </div>
