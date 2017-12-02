9d7d99e4bdade7bb5a54c20bb6226e532ddc0a30
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2017-12-02 09:23:40 +0100

    front-page.php
    include template parts feature and utility 1

--- front-page.php
+++ front-page.php
@@ -2,5 +2,7 @@
 
 <?php get_template_part('template-parts/content', 'home-banner'); ?>
 <?php get_template_part('template-parts/content', 'home-service'); ?>
+<?php get_template_part('template-parts/content', 'home-feature'); ?>
+<?php get_template_part('template-parts/content', 'home-utility-1'); ?>
 
 <?php get_footer('home'); ?>
