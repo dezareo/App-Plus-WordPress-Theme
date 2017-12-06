9f8c866a08eec0f02168ba005599e6dd72022d68
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2017-12-06 15:05:09 +0100

    create footer-home.php with ACF implementation

--- footer-home.php
+++ footer-home.php
@@ -1,3 +1,21 @@
+<?php
+$footer_logo = get_field('footer_logo');
+$footer_copyright = get_field('footer_copyright');
+?>
+
+    <footer class="wow fadeInUp" data-wow-delay=".8s">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-12">
+                        <a class="footer-logo" href="#">
+                            <img class="img-responsive" src="<?php echo $footer_logo; ?>" alt="">
+                        </a>
+                    <p><?php echo $footer_copyright; ?></p>
+                    
+                </div>
+            </div>
+        </div>
+    </footer>
 
 
 </div><!-- #page -->
